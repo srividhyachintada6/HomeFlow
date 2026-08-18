5619286672d996385b8919ec0ddd2334348a9945
Srividhya6 <srividhyachintada6@gamil.com>
2026-08-18 11:09:02 +0530

    Fix GitHub Pages React routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,10 +7,10 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <BrowserRouter basename="/HomeFlow">
+  <AuthProvider>
+    <App />
+  </AuthProvider>
+</BrowserRouter>
   </StrictMode>,
 )
